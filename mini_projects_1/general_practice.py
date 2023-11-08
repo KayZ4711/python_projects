ee4271612f621132cb4ecfd7402995916eb29f5e
Ahmad Kais Sadat <ahmadks@uia.no>
2023-11-08 02:49:28 +0100

    General practice, continuation

--- mini_projects_1/general_practice.py
+++ mini_projects_1/general_practice.py
@@ -68,37 +68,102 @@
 # Score 60 to 69: Grade D
 # Score below 60: Grade F
 
-score = input("What was your test score? ")
-score = int(score)
+# score = input("What was your test score? ")
+# score = int(score)
 
-if score >= 90:
-    age = int(input("What is your age?"))
-    if age < 12:
-        print("Your grade is an A+.")
-    else:
-        print("Your grade is an A.")
+# if score >= 90:
+#     age = int(input("What is your age?"))
+#     if age < 12:
+#         print("Your grade is an A+.")
+#     else:
+#         print("Your grade is an A.")
 
-elif score >= 80:
-    age = int(input("What is your age?"))
-    if age < 12:
-        print("Your grade is an B+.")
-    else:
-        print("Your grade is a B.")
+# elif score >= 80:
+#     age = int(input("What is your age?"))
+#     if age < 12:
+#         print("Your grade is an B+.")
+#     else:
+#         print("Your grade is a B.")
 
-elif score >= 70:
-    age = int(input("What is your age?"))
-    if age < 12:
-        print("Your grade is an C+.")
-    else:
-        print("Your grade is a C.")
+# elif score >= 70:
+#     age = int(input("What is your age?"))
+#     if age < 12:
+#         print("Your grade is an C+.")
+#     else:
+#         print("Your grade is a C.")
 
-elif score >= 60:
-    age = int(input("What is your age?"))
-    if age < 12:
-        print("Your grade is an D+.")
-    else: 
-        print("Your grade is a D.")
+# elif score >= 60:
+#     age = int(input("What is your age?"))
+#     if age < 12:
+#         print("Your grade is an D+.")
+#     else: 
+#         print("Your grade is a D.")
 
-else: 
-    print("Next time study more!")
+# else: 
+#     print("Next time study more!")
     
+#########################################################################################################
+
+# fruits = ["apple", "banana", "cherry"]
+
+# for i in fruits:
+#     print(i)
+
+# for num in range (1,100):
+#     if num % 2 == 0:
+#        print(num)
+
+
+
+# Fizzbuzz 
+# Write a program that loops through the numbers 1-100 and then 
+# If a number is divisible by 3, print fizz
+# If a number is divisible by 5, print buzz
+# If a number is divisible by both, print fizzbuzz
+
+# for num in range(1,100):
+#     if num % 3 == 0 and num % 5 == 0:
+#         print("fizzbuzz")
+#     elif num % 3 == 0:
+#         print("fizz")
+#     elif num % 5 == 0:
+#         print("buzz")
+#     else:
+#         print(num)
+
+
+#########################################################################################################
+
+# Creating our own functions 
+# import time 
+
+# choice = int(input("What is your choice? "))
+
+# def function(choice):
+#     for num in range(1, choice):
+#         if num % 3 == 0 and num % 5 == 0:
+#             print("FizzBuzz")
+#         elif num % 3 == 0:
+#             print("Fizz")
+#         elif num % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print(num)
+
+# print("Running the program . . . ")
+# time.sleep(5)
+
+# function(choice)
+
+#########################################################################################################
+
+# Create a program that can take in input of the users name
+# save the name in a variable
+# pass the variable through a function and print "Hello _____"
+
+user_name = str(input("What is your name? "))
+
+def function(user_name):
+    print(f"Hello {user_name}")
+
+function(user_name)
